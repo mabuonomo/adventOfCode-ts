611f688b37b511b71ec91f0f5e520a832e826c6a
mario <mario@localhost.localdomain>
2018-12-09 15:46:52 +0100

    cosmetic changes

--- src/day9/common.ts
+++ src/day9/common.ts
@@ -1,11 +1,10 @@
 import { NodeElm, Game } from "./types";
 
 export function game(properties: Game): number {
-    const res = [];
-    for (let i = 1; i <= properties.players; i += 1) {
-        res[i] = 0;
-    }
-    let currentPlayer = 1;
+    let res: Array<number> = new Array<number>(properties.players + 1);
+    res.fill(0);
+
+    let player = 1;
 
     let current: NodeElm = {
         value: 0,
@@ -15,28 +14,27 @@ export function game(properties: Game): number {
 
     for (let i = 0; i < properties.marbles; i++) {
         if (i % 23 === 0) {
-            res[currentPlayer] += i;
+            res[player] += i;
             current = current.prev!.prev!.prev!.prev!.prev!.prev!;
-            res[currentPlayer] += current.prev!.value;
+            res[player] += current.prev!.value;
             current.prev!.prev!.next = current;
             current.prev = current.prev!.prev;
-
         } else {
-            current = addAfter(i, current.next!);
+            current = addNode(i, current.next!);
         }
-        currentPlayer = currentPlayer % properties.players + 1;
+        player = player % properties.players + 1;
     }
 
     return Math.max(...Object.values(res));
 }
 
-function addAfter(value: number, marble: NodeElm) {
+function addNode(value: number, marble: NodeElm) {
     let newNode: NodeElm = {
         value,
         prev: marble,
         next: marble.next,
     };
-    
+
     marble.next!.prev = newNode;
     marble.next = newNode;
 
