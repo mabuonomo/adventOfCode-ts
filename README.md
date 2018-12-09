9b59f1988d51f3526d855bc7a1a06fc9eb301e0a
mario <mario@localhost.localdomain>
2018-12-09 10:31:21 +0100

    rename files

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ docker-compose build
 
 ## Run
 ```bash
-docker-compose run app ts-node src/{day}/{part}.ts
+docker-compose run app ts-node src/{day}/part{1 or 2}.ts
 ```
 Example
 ```bash
-docker-compose run app ts-node src/day1/day1_1.ts
+docker-compose run app ts-node src/day1/part1.ts
 ```
---
+++ src/day9/part1.ts
@@ -0,0 +1,65 @@
+import * as fs from 'fs';
+import * as rd from 'readline'
+
+var reader = rd.createInterface(fs.createReadStream("./src/day9/input.txt"))
+
+type Game = { players: number, marbles: number };
+type Node = { value: number, next: Node, prev?: Node };
+
+let properties: Game;
+
+reader.on("line", (l: string) => {
+    let tokens = l.split(' ');
+
+    properties = { players: parseInt(tokens[0]), marbles: parseInt(tokens[6]) };
+});
+
+reader.on("close", () => {
+
+    console.log(properties);
+
+    let t = new Date().getTime();
+    // console.log('Result ' + walk());
+    console.log('Timing: ' + (new Date().getTime() - t) + ' ms');
+})
+
+function game() {
+    const scores: Array<number> = [];
+
+    for (let i = 1; i <= properties.players; i += 1) {
+        scores[i] = 0;
+    }
+    let currentPlayer = 1;
+
+    let current: Node = {
+        value: 0
+    };
+    current.next = current;
+    current.prev = current;
+
+    for (let m = 1; m <= properties.marbles * 100; m += 1) {
+        if (m % 23 === 0) {
+            scores[currentPlayer] += m;
+            current = current.prev.prev.prev.prev.prev.prev;
+            scores[currentPlayer] += current.prev.value;
+            current.prev.prev.next = current;
+            current.prev = current.prev.prev;
+        } else {
+            current = addAfter(m, current.next);
+        }
+        currentPlayer = currentPlayer % playerCount + 1;
+    }
+
+    return Math.max(...Object.values(scores));
+}
+
+const addAfter = (value: number, marble: number) => {
+    const toAdd = {
+        value,
+        prev: marble,
+        next: marble.next,
+    };
+    marble.next.prev = toAdd;
+    marble.next = toAdd;
+    return toAdd;
+};
