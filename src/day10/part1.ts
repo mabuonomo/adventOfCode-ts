420c265fcf4f0d5405bbaffe6c28fd14a80cd859
Mario Buonomo <mario.buonomo@facile.it>
2018-12-10 17:42:41 +0100

    improve

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -7,25 +7,45 @@ var reader = rd.createInterface(fs.createReadStream("./src/day10/input.txt"))
 type Position = { x: number, y: number };
 type Velocity = { x: number, y: number };
 
-type Point = {position: Position, velocity : Velocity};
+type Point = { start: Position, velocity: Velocity, actual: Position };
 
-let points : Array<Point> = [];
+let points: Array<Point> = [];
 // position=< 10546,  31107> velocity=<-1, -3>
 reader.on("line", (l: string) => {
     let xP = parseInt(l.split(',')[0].split('position=<')[1]);
     let yP = parseInt(l.split(',')[1].split('>')[0]);
-    let position = {x:xP, y:yP};
+    let position = { x: xP, y: yP };
 
     let xV = parseInt(l.split('velocity=<')[1].split(',')[1]);
     let yV = parseInt(l.split('velocity=<')[1].split(',')[0].split('>')[0]);
-    let velocity = {x:xV, y:yV};
+    let velocity = { x: xV, y: yV };
 
-    points.push({position: position, velocity:velocity});
+    points.push({ start: position, velocity: velocity, actual: position });
 });
 
 reader.on("close", () => {
     let t = new Date().getTime();
 
+    let [maxX, maxY] = getSize();
+    console.log("Max: " + maxX + " " + maxY);
+
     console.log('Timing: ' + (new Date().getTime() - t) + ' ms');
 })
+
+function getSize(): [number, number] {
+    let maxX = 0;
+    let maxY = 0;
+    points.forEach(element => {
+        if (element.start.x > maxX) {
+            maxX = element.start.x;
+        }
+
+        if (element.start.y > maxY) {
+            maxY = element.start.x;
+        }
+    });
+
+    return [maxX, maxY];
+}
+
 
