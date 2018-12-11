6a514bd5ed1316a794e1f014a8200ef9c8263cf0
mario <mario@localhost.localdomain>
2018-12-11 20:47:35 +0100

    test ok

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -15,64 +15,112 @@ reader.on("line", (l: string) => {
     let xP = parseInt(l.split(',')[0].split('position=<')[1]);
     let yP = parseInt(l.split(',')[1].split('>')[0]);
     let position = { x: xP, y: yP };
+    let actual = { x: xP, y: yP };
 
     let xV = parseInt(l.split('velocity=<')[1].split(',')[1]);
     let yV = parseInt(l.split('velocity=<')[1].split(',')[0].split('>')[0]);
-    let velocity = { x: xV, y: yV };
+    let velocity = { x: yV, y: xV };
 
-    points.push({ start: position, velocity: velocity, actual: position });
+    points.push({ start: position, velocity: velocity, actual: actual });
 });
 
 reader.on("close", () => {
     let t = new Date().getTime();
 
-    let [maxX, maxY] = getSize();
-    console.log("Max: " + maxX + " " + maxY);
+    console.log(points);
+    // return; 
+    // print(buildMatrix(minX, minY), maxX, maxY, minX, minY);
 
-    let [minX, minY] = getMin();
-    console.log("Min: " + minX + " " + minY);
-
-    print(buildMatrix(minX, minY), maxX, maxY, minX, minY);
-
+    movePoints();
     console.log('Timing: ' + (new Date().getTime() - t) + ' ms');
 })
 
 let reduce = 1;
-let dimPixel = 700;//550;
+
 function buildMatrix(minX: number, minY: number): Array<Array<string>> {
     let matrix: Array<Array<string>> = [];
 
+    let count = 0;
     points.forEach(element => {
-        let x = (element.actual.x + minX) / reduce;
-        let y = (element.actual.y + minY) / reduce;
+        let x = element.actual.x + Math.abs(minX);
+        let y = element.actual.y + Math.abs(minY);
 
         if (matrix[x] === undefined) {
             matrix[x] = [];
         }
 
         matrix[x][y] = '#';
+        count++;
     });
 
+    console.log('Punti inseriti in matrix: ' + count)
+    // console.log(matrix);
     return matrix;
 }
 
+function print(matrix: Array<Array<string>>) {
+    let count = 0;
+    for (let x = 0; x <= points.length; x++) {
+        for (let y = 0; y <= points.length; y++) {
+            if (matrix[x] === undefined || matrix[x][y] === undefined) {
+                process.stdout.write('.');
+            } else {
+                process.stdout.write('#');
+                count++;
+            }
+        }
+
+        process.stdout.write('\n');
+    }
+    // process.stdout.write('\n\n');
+
+    console.log('Punti stampati a video: ' + count);
+    process.stdout.write('\n\n');
+}
+
 function movePoints(s: number = 1) {
+
     points.forEach(element => {
-        element.start.x *= (element.velocity.x * s);
-        element.start.y *= (element.velocity.y * s);
+        element.actual.x = element.start.x + (element.velocity.x * s);
+        element.actual.y = element.start.y + (element.velocity.y * s);
     });
+
+    // console.log(points);
+    // return;
+
+    // if (s == 3) return;
+
+    let [maxX, maxY] = getSize();
+    // console.log("Max: " + maxX + " " + maxY);
+
+    let [minX, minY] = getMin();
+    // console.log("Min: " + minX + " " + minY);
+
+    // console.log("Points: " + points.length);
+
+    // console.log(Math.abs(maxX - minX));
+    // return;
+
+    if (Math.abs(maxX - minX) <= points.length && Math.abs(maxY - minY) <= points.length) {
+        console.log('Tento s: ' + s);
+        let matrix = buildMatrix(minX, minY);
+        print(matrix);
+        movePoints(++s);
+    } else {
+        return
+    }
 }
 
 function getSize(): [number, number] {
     let maxX = 0;
     let maxY = 0;
     points.forEach(element => {
-        if (element.start.x > maxX) {
-            maxX = element.start.x;
+        if (element.actual.x > maxX) {
+            maxX = element.actual.x;
         }
 
-        if (element.start.y > maxY) {
-            maxY = element.start.y;
+        if (element.actual.y > maxY) {
+            maxY = element.actual.y;
         }
     });
 
@@ -83,12 +131,12 @@ function getMin(): [number, number] {
     let minX = 0;
     let minY = 0;
     points.forEach(element => {
-        if (element.start.x < minX) {
-            minX = element.start.x;
+        if (element.actual.x < minX) {
+            minX = element.actual.x;
         }
 
-        if (element.start.y < minY) {
-            minY = element.start.y;
+        if (element.actual.y < minY) {
+            minY = element.actual.y;
         }
     });
 
