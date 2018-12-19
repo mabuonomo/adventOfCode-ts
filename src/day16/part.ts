f7e53fd2cc8de823f51023533c9c0b4e4f851877
mario <mario@localhost.localdomain>
2018-12-19 21:35:11 +0100

    day 16 part 1

--- src/day16/part.ts
+++ src/day16/part.ts
@@ -1,123 +1,103 @@
-const fs16 = require('fs')
-const input16 = String(fs16.readFileSync(__dirname + '/input.txt')).trim()
-console.log(__dirname + '/input.txt');
+let fss = require("fs");
+let paths = require('path');
+let filepaths = paths.join(__dirname, "./input.txt");
 
-function range(start, stop, step = 1) {
-    const r = []
-    for (let i = start; i < stop; i += step) {
-        r.push(i)
+const ops = [
+    (r, a, b, c) => { r[c] = r[a] + r[b] },
+    (r, a, b, c) => { r[c] = r[a] + b },
+    (r, a, b, c) => { r[c] = r[a] * r[b] },
+    (r, a, b, c) => { r[c] = r[a] * b },
+    (r, a, b, c) => { r[c] = r[a] & r[b] },
+    (r, a, b, c) => { r[c] = r[a] & b },
+    (r, a, b, c) => { r[c] = r[a] | r[b] },
+    (r, a, b, c) => { r[c] = r[a] | b },
+    (r, a, b, c) => { r[c] = r[a] },
+    (r, a, b, c) => { r[c] = a },
+    (r, a, b, c) => { r[c] = a > r[b] ? 1 : 0 },
+    (r, a, b, c) => { r[c] = r[a] > b ? 1 : 0 },
+    (r, a, b, c) => { r[c] = r[a] > r[b] ? 1 : 0 },
+    (r, a, b, c) => { r[c] = a === r[b] ? 1 : 0 },
+    (r, a, b, c) => { r[c] = r[a] === b ? 1 : 0 },
+    (r, a, b, c) => { r[c] = r[a] === r[b] ? 1 : 0 }
+]
+
+let r = [0, 0, 0, 0]
+let result = []
+let count = 0
+
+let possibilities = []
+let pos = []
+for (let i = 0; i < 16; i++) {
+    pos.push(i)
+}
+for (let i = 0; i < 16; i++) {
+    possibilities.push(new Set(pos))
+}
+let opcode = 0
+
+for (const line of fss.readFileSync(filepaths, "utf-8").trimEnd().split('\n')) {
+    let m = line.match(/Before: \[(\d+), (\d+), (\d+), (\d+)\]/)
+    if (m) {
+        r = m.slice(1).map(n => Number(n))
     }
-    return r
-}
-
-const inputOperationsRaw = input16.split('\n')
-const lastAfter = inputOperationsRaw.filter(l => l.indexOf('After') === 0).length
-const inputOperations = inputOperationsRaw.slice(0, lastAfter * 4)
-
-
-const inputOps = range(0, inputOperations.length, 4).reduce(
-    (accl, i) => {
-        return accl.concat({
-            before: inputOperationsRaw[i].split('[')[1].substr(0, 10).split(', ').map(c => parseInt(c)),
-            op: {
-                op: parseInt(inputOperationsRaw[i + 1].split(' ')[0]),
-                a: parseInt(inputOperationsRaw[i + 1].split(' ')[1]),
-                b: parseInt(inputOperationsRaw[i + 1].split(' ')[2]),
-                c: parseInt(inputOperationsRaw[i + 1].split(' ')[3])
-            },
-            after: inputOperationsRaw[i + 2].split('[')[1].substr(0, 10).split(', ').map(c => parseInt(c))
-        })
-    }, [])
-
-const eq = (a, b) => a.length === b.length && a.reduce((e, v, i) => e && (v === b[i]), true)
-
-
-const ops = {
-    addr: (a, b, c, regs) => { regs[c] = regs[a] + regs[b] },
-    addi: (a, b, c, regs) => { regs[c] = regs[a] + b },
-    mulr: (a, b, c, regs) => { regs[c] = regs[a] * regs[b] },
-    muli: (a, b, c, regs) => { regs[c] = regs[a] * b },
-    banr: (a, b, c, regs) => { regs[c] = regs[a] & regs[b] },
-    bani: (a, b, c, regs) => { regs[c] = regs[a] & b },
-    borr: (a, b, c, regs) => { regs[c] = regs[a] | regs[b] },
-    bori: (a, b, c, regs) => { regs[c] = regs[a] | b },
-    setr: (a, b, c, regs) => { regs[c] = regs[a] },
-    seti: (a, b, c, regs) => { regs[c] = a },
-    gtir: (a, b, c, regs) => { regs[c] = a > regs[b] ? 1 : 0 },
-    gtri: (a, b, c, regs) => { regs[c] = regs[a] > b ? 1 : 0 },
-    gtrr: (a, b, c, regs) => { regs[c] = regs[a] > regs[b] ? 1 : 0 },
-    eqir: (a, b, c, regs) => { regs[c] = a === regs[b] ? 1 : 0 },
-    eqri: (a, b, c, regs) => { regs[c] = regs[a] === b ? 1 : 0 },
-    eqrr: (a, b, c, regs) => { regs[c] = regs[a] === regs[b] ? 1 : 0 },
-}
-const opNames = Object.keys(ops)
-
-const testOps = opNames.reduce((tests: any, op) => ({
-    ...tests,
-    [op]: (a, b, c, before, after) => {
-        const reg = before.slice()
-        ops[op](a, b, c, reg)
-        return eq(reg, after)
-    }
-}), {})
-
-function posibilities() {
-    const possibleMappings = []
-    inputOps.forEach(({ after, before, op }) => {
-        const matching = opNames
-            .filter((opName) => {
-                return testOps[opName](op.a, op.b, op.c, before, after)
-            })
-
-        if (!possibleMappings[op.op]) {
-            possibleMappings[op.op] = new Set(matching)
-        } else {
-            possibleMappings[op.op] = intersection(
-                possibleMappings[op.op], new Set(matching)
-            )
-        }
-    })
-    return possibleMappings
-}
-
-const result1 = posibilities().filter(s => s.size >= 3).length
-
-function intersection(set1, set2) {
-    var s = new Set()
-    for (var elem of set2) {
-        if (set1.has(elem)) {
-            s.add(elem)
+    m = line.match(/^(\d+) (\d+) (\d+) (\d+)/)
+    if (m) {
+        m = m.slice(1).map(n => Number(n))
+        opcode = m[0]
+        result = []
+        for (const op of ops) {
+            let res = [...r]
+            op(res, m[1], m[2], m[3])
+            result.push(res)
         }
     }
-    return s
-}
-
-function findMapping() {
-    const foundMapping = {}
-    while (Object.keys(foundMapping).length !== Object.keys(ops).length) {
-        const options = posibilities()
-            .map(s => Array.from(s).filter(
-                op => Object.values(foundMapping).indexOf(op) === -1))
-
-        const matchName = options.filter(l => l.length === 1)[0][0]
-        const matchIndex = options.findIndex(l => l.length === 1)
-
-        foundMapping[matchIndex] = matchName
+    m = line.match(/After: *\[(\d+), (\d+), (\d+), (\d+)\]/)
+    if (m) {
+        const t = m.slice(1).map(n => Number(n))
+        let c = 0
+        let i = 0
+        for (const res of result) {
+            if (res.every((cur, idx) => cur === t[idx])) {
+                c++
+            } else {
+                possibilities[opcode].delete(i)
+            }
+            i++
+        }
+        if (c >= 3) {
+            count++
+        }
     }
-
-    return foundMapping
 }
-const finalMapping = findMapping()
 
-const program = inputOperationsRaw.slice(lastAfter * 4 + 1)
-    .filter(l => l.length > 3)
-    .map(l => l.split(' ').map(c => parseInt(c)))
+console.log(count)
 
-const lastRegisters = program.reduce((regs, op) => {
-    ops[finalMapping[op[0]]](op[1], op[2], op[3], regs)
-    return regs
-}, new Array(4).fill(0))
+let todo = new Set()
+for (let i = 0; i < 16; i++) {
+    todo.add(i)
+}
 
+let optable = []
+while (todo.size > 0) {
+    for (const i of todo) {
+        if (possibilities[i].size === 1) {
+            optable[i] = [...possibilities[i]][0]
+            todo.delete(i)
+            for (let j = 0; j < 16; j++) {
+                possibilities[j].delete(optable[i])
+            }
+        }
+    }
+}
 
-console.log(`Part 1: ${result1}`)
-console.log(`Part 2: ${lastRegisters[0]}`)
+r = [0, 0, 0, 0]
+
+fss = require("fs");
+paths = require('path');
+filepaths = paths.join(__dirname, "./input.txt");
+
+for (const line of fss.readFileSync(filepaths, "utf-8").trimEnd().split('\n').slice(3343)) {
+    const [, op, a, b, c] = line.match(/(\d+) (\d+) (\d+) (\d+)/).map(n => Number(n))
+    ops[optable[op]](r, a, b, c)
+}
+console.log(r)
