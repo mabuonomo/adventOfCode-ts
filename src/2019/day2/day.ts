1071815b55bd157c37fd66d6e9b0ec9ea3707849
Mario Buonomo <mario.buonomo@facile.it>
2019-12-02 13:58:16 +0100

    day 2

---
+++ src/2019/day2/day.ts
@@ -0,0 +1,54 @@
+import { InitAbstract } from '../init.abstract';
+import { performanceLog } from 'decorators-utils-ts/dist/src';
+
+class Day extends InitAbstract {
+  lines: Array<string>;
+
+  reg: Array<number> = [];
+
+  constructor() {
+    super();
+
+    this.lines = this.getLines('day2', false);
+
+    this.lines[0].split(',').forEach((element) => {
+      this.reg.push(parseInt(element.trim()));
+    });
+  }
+
+  @performanceLog(true)
+  runPart1(): number {
+    this.reg[1] = 12;
+    this.reg[2] = 2;
+
+    for (let i = 0; i < this.reg.length; i += 4) {
+      switch (this.reg[i]) {
+        case 1:
+          let add1 = this.reg[this.reg[i + 1]];
+          let add2 = this.reg[this.reg[i + 2]];
+          this.reg[this.reg[i + 3]] = add1 + add2;
+          break;
+        case 2:
+          let mul1 = this.reg[this.reg[i + 1]];
+          let mul2 = this.reg[this.reg[i + 2]];
+          this.reg[this.reg[i + 3]] = mul1 * mul2;
+          break;
+        case 99:
+          return this.reg[0];
+        default:
+          console.log('Attention code not recognized');
+      }
+    }
+
+    return this.reg[0];
+  }
+
+  @performanceLog(true)
+  runPart2(): number {
+    return 0;
+  }
+}
+
+let day = new Day();
+day.runPart1();
+day.runPart2();
