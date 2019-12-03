96901304979698b2be6f55b14a522f082db68c44
Mario Buonomo <ma.buonomo@gmail.com>
2019-12-03 22:14:09 +0100

    test

--- src/2019/day3/day.spec.ts
+++ src/2019/day3/day.spec.ts
@@ -5,21 +5,4 @@ test('Build commands', () => {
 
   let line = 'R8,U5,L5,D3';
   let commands = c.buildDirection(line);
-
-  expect(commands).toEqual([
-    { direction: 'R', value: 8 },
-    { direction: 'U', value: 5 },
-    { direction: 'L', value: 5 },
-    { direction: 'D', value: 3 },
-  ]);
-
-  let points = c.buildPoint(commands);
-
-  expect(points).toEqual([
-    { x: 0, y: 0 },
-    { x: 8, y: 0 },
-    { x: 8, y: 5 },
-    { x: 3, y: 5 },
-    { x: 3, y: 2 },
-  ]);
 });
