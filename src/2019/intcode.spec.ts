271ce2ca34bed6607407c9f4940748500d0e20b9
Mario Buonomo <ma.buonomo@gmail.com>
2019-12-07 21:10:42 +0100

    reafactor

--- src/2019/intcode.spec.ts
+++ src/2019/intcode.spec.ts
@@ -35,3 +35,64 @@ test('Fake test 5', () => {
   let d = new IntCode([3, 0, 4, 0, 99], 0);
   expect(d.run()).toEqual(0);
 });
+
+test('Fake test 7', () => {
+  let inp = [
+    3,
+    21,
+    1008,
+    21,
+    8,
+    20,
+    1005,
+    20,
+    22,
+    107,
+    8,
+    21,
+    20,
+    1006,
+    20,
+    31,
+    1106,
+    0,
+    36,
+    98,
+    0,
+    0,
+    1002,
+    21,
+    125,
+    20,
+    4,
+    20,
+    1105,
+    1,
+    46,
+    104,
+    999,
+    1105,
+    1,
+    46,
+    1101,
+    1000,
+    1,
+    20,
+    4,
+    20,
+    1105,
+    1,
+    46,
+    98,
+    99,
+  ];
+
+  let c = new IntCode(inp, 7);
+  expect(c.run()).toEqual(999);
+
+  let d = new IntCode(inp, 8);
+  expect(d.run()).toEqual(1000);
+
+  let e = new IntCode(inp, 9);
+  expect(e.run()).toEqual(1001);
+});
--- src/2019/intcode.ts
+++ src/2019/intcode.ts
@@ -1,132 +1,97 @@
 export class IntCode {
-  registry: Array<number>;
-  input: number;
+    registry: Array<number>;
+    input: number;
 
-  constructor(registry: Array<number>, input: number) {
-    this.registry = registry;
-    this.input = input;
-  }
-
-  getRegistry() {
-    return this.registry;
-  }
-
-  run(): number {
-    for (let i = 0; i < this.registry.length; i++) {
-      let res = this.execute(i);
-
-      if (res.output !== undefined) return res.output;
-
-      i += res.incr;
+    constructor(registry: Array<number>, input: number) {
+        this.registry = registry;
+        this.input = input;
     }
 
-    return undefined;
-  }
-
-  execute(i: number): Result {
-    let op = this.buildOP(i);
-
-    if (op == undefined) {
-      return { res: false, incr: 0, output: undefined };
+    getRegistry() {
+        return this.registry;
     }
 
-    let param1 = 0;
-    let param2 = 0;
+    run(): number {
+        for (let i = 0; i < this.registry.length; i++) {
+            let res = this.execute(i);
 
-    if (op.first == Mode.POSITION) {
-      param1 = this.registry[this.registry[i + 1]];
-    } else {
-      param1 = this.registry[i + 1];
-    }
+            // console.log(res)
+            if (res.output !== undefined) return res.output;
 
-    if (op.second == Mode.POSITION) {
-      param2 = this.registry[this.registry[i + 2]];
-    } else {
-      param2 = this.registry[i + 2];
-    }
-
-    // console.log(param1, param2)
-
-    switch (op.code) {
-      case 1:
-        let add1 = param1;
-        let add2 = param2;
-        if (op.third == Mode.POSITION) {
-          this.registry[this.registry[i + 3]] = add1 + add2;
-        } else {
-          this.registry[i + 3] = add1 + add2;
+            i += res.incr;
         }
-        return { res: false, incr: 4, output: undefined };
-      case 2:
-        let mul1 = param1;
-        let mul2 = param2;
-        if (op.third == Mode.POSITION) {
-          this.registry[this.registry[i + 3]] = mul1 * mul2;
-        } else {
-          this.registry[i + 3] = mul1 * mul2;
+
+        return undefined;
+    }
+
+    execute(i: number): Result {
+        let op = this.buildOP(i);
+
+        if (op == undefined) {
+            return { res: false, incr: 0, output: undefined };
         }
-        return { res: false, incr: 4, output: undefined };
-      case 3:
-        if (op.first == Mode.POSITION) {
-          this.registry[this.registry[i + 1]] = this.input;
-        } else {
-          this.registry[i + 1] = this.input;
+
+        let param1 = op.first == Mode.POSITION ? this.registry[this.registry[i + 1]] : this.registry[i + 1];
+        let param2 = op.second == Mode.POSITION ? this.registry[this.registry[i + 2]] : this.registry[i + 2];
+
+        switch (op.code) {
+            case 1:
+                this.registry[this.registry[i + 3]] = param1 + param2;
+                return { res: false, incr: 4, output: undefined };
+            case 2:
+                this.registry[this.registry[i + 3]] = param1 * param2;
+                return { res: false, incr: 4, output: undefined };
+            case 3:
+                this.registry[this.registry[i + 1]] = this.input;
+                return { res: false, incr: 1, output: undefined };
+            case 4:
+                return { res: true, incr: 0, output: param1 };
+            case 99:
+                return { res: true, incr: 0, output: this.input };
         }
-        return { res: false, incr: 1, output: undefined };
-      case 4:
-        let res = 0;
-        if (op.first == Mode.POSITION) {
-          res = this.registry[this.registry[i + 1]];
-        } else {
-          res = this.registry[i + 1];
+
+        return { res: false, incr: 0, output: undefined };
+    }
+
+    buildOP(i: number): IntOP {
+        let op = this.registry[i].toString();
+
+        if (op.length === 4 && parseInt(op.charAt(0)) < 2 && parseInt(op.charAt(1)) < 2) {
+            op = '0' + op;
         }
-        return { res: true, incr: 1, output: res };
-      case 99:
-        return { res: true, incr: 0, output: this.input }; // this.registry[0] };
+
+        if (op.length === 3 && parseInt(op.charAt(0)) < 2) {
+            op = '00' + op;
+        }
+
+        if (op.length === 2 && parseInt(op) == 99) {
+            op = '000' + op;
+        }
+
+        if (op.length !== 5) {
+            return undefined;
+        }
+
+        return {
+            third: this.getMode(parseInt(op.charAt(0))),
+            second: this.getMode(parseInt(op.charAt(1))),
+            first: this.getMode(parseInt(op.charAt(2))),
+            code: parseInt(op.substring(3)),
+        };
     }
 
-    return { res: false, incr: 0, output: undefined };
-  }
+    getMode(n: number) {
+        if (n == 0) return Mode.POSITION;
 
-  buildOP(i: number): IntOP {
-    let op = this.registry[i].toString();
-
-    if (op.length < 2) {
-      return undefined;
+        return Mode.IMMEDIATE;
     }
-
-    if (op.length === 4) {
-      op = '0' + op;
-    }
-
-    if (op.length === 3) {
-      op = '00' + op;
-    }
-
-    if (op.length === 2) {
-      op = '000' + op;
-    }
-
-    return {
-      third: this.getMode(parseInt(op.charAt(0))),
-      second: this.getMode(parseInt(op.charAt(1))),
-      first: this.getMode(parseInt(op.charAt(2))),
-      code: parseInt(op.substring(3)),
-    };
-  }
-
-  getMode(n: number) {
-    if (n == 0) return Mode.POSITION;
-
-    return Mode.IMMEDIATE;
-  }
 }
 
 type IntOP = { third: Mode; second: Mode; first: Mode; code: number };
 
 enum Mode {
-  POSITION, // by address
-  IMMEDIATE, // by value
+    POSITION, // by address
+    IMMEDIATE, // by value
 }
 
 type Result = { res: boolean; incr: number; output: number };
